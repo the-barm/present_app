23cf0ace938e03229e83ae22f3c1461fdb62fa49
the-barm <funfundzwanzigsten@gmail.com>
2015-08-04 16:12:28 +0000

    Remembering users, login form fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150802160955) do
+ActiveRecord::Schema.define(version: 20150804143048) do
 
   create_table "pictures", force: :cascade do |t|
     t.datetime "created_at",        null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150802160955) do
     t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
