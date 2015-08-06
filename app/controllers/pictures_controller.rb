ac83b24882993ad89d1785e571a221f492a72604
the-barm <funfundzwanzigsten@gmail.com>
2015-08-06 19:33:46 +0000

    Denied access for guests, made edit account form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,20 @@
 class PicturesController < ApplicationController
+  before_action :logged_in_user, only: [:new, :index]
+
   def new
   end
   
   def index
     @pictures = Picture.all
   end
+  
+  private 
+  
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,7 +7,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   
   test "login with valid information followed by logout" do
     get login_path
-    post login_path, session: { email: @user.email, password: 'password' }
+    log_in_as(@user)
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
