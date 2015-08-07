8b9b4a0e5c84bedc508376db48ea362979f952ee
the-barm <funfundzwanzigsten@gmail.com>
2015-08-07 22:35:59 +0000

    Prohibited uploading for non-logged-in users

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,14 +7,4 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
-  
-  private 
-  
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 end
--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,4 +1,5 @@
 class UploadsController < ApplicationController
+  before_action :logged_in_user, only: [:create]
   
   def create
     @upload = Upload.new(upload_params)
