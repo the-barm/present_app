6cfd753f01cc754082642b391b670db1c29fb7e1
the-barm <funfundzwanzigsten@gmail.com>
2015-08-16 18:58:50 +0000

    Added user input in /letters

--- app/assets/javascripts/letters.js
+++ app/assets/javascripts/letters.js
@@ -1,9 +1,7 @@
-var animate_letters = function() {
-  var $phrase = 'abcdefghijklmnopqrstuvwxyz';
-  for (var i = 0; i < $phrase.length; i++) {
-        $('div#lgn-button').append($('<div class = "letters"/>').text($phrase[i]));
-    }
-    
+var fill_and_animate = function(phrase) {
+  for (var i = 0; i < phrase.length; i++) {
+        $('div#lgn-button').append($('<div class = "letters"/>').text(phrase[i]));
+  };
   $('div.letters').mouseenter(function() {
        $(this).animate({
           fontSize: '4em'
@@ -15,6 +13,20 @@ var animate_letters = function() {
        }); 
   });
 };
+
+
+
+var animate_letters = function() {
+  var phrase = 'abcdefghijklmnopqrstuvwxyz';
+  fill_and_animate(phrase);
+  
+  $('.btn').click( function() {
+    phrase = $('input[name=checkListItem]').val();
+    $('div.letters').remove();
+    fill_and_animate(phrase);
+  });
+};
+
 
 
 $(document).ready(animate_letters);
